6e02440f8d6b4df83a8a20c759c34dac13cc337c
moxi.feng <moxi.feng@advantest.com>
2022-08-30 17:38:18 +0800

    feat: 新增menu菜单的抽象

--- src/components/Dropdown/src/Dropdown.tsx
+++ src/components/Dropdown/src/Dropdown.tsx
@@ -1,8 +1,61 @@
-// import { Dropdown, Menu, Popconfirm } from 'antd'
-// import type { DropMenu } from './typing'
+import {
+    Dropdown,
+    Menu,
+    // Popconfirm
+} from 'antd'
+import type { DropMenu } from './typing'
+import React from 'react'
+import PropTypes from 'prop-types'
+// import { omit } from 'lodash-es'
+import { MenuInfo } from 'rc-menu/lib/interface'
 
-// const Dropdown = () => {
-//     return <></>
-// }
+// const { Item, Divider } = Menu
 
-// export default Dropdown
+interface DropdownComponentI {
+    dropMenuList: (DropMenu & Recordable)[]
+    trigger: 'click' | 'hover'
+    selectedKeys: string[]
+    popConfirmStatus: boolean
+}
+
+const DropdownComponent = ({
+    dropMenuList,
+    trigger,
+    selectedKeys,
+    popConfirmStatus,
+}: DropdownComponentI) => {
+    const handleClickMenu = ({ item, key, keyPath, domEvent }: MenuInfo): void => {
+        console.log(item, key, keyPath, domEvent, selectedKeys, popConfirmStatus)
+    }
+
+    const menu = (
+        <Menu
+            items={dropMenuList?.map(ci => ({
+                key: ci?.event,
+                disabled: ci?.disabled,
+                icon: ci?.popConfirm?.icon,
+            }))}
+            onClick={handleClickMenu}
+            triggerSubMenuAction={trigger}
+        />
+    )
+
+    return (
+        <>
+            <Dropdown overlay={menu}>
+                <span>
+                    <slot></slot>
+                </span>
+            </Dropdown>
+        </>
+    )
+}
+
+DropdownComponent.PropTypes = {
+    dropMenuList: PropTypes.array.isRequired,
+    trigger: PropTypes.arrayOf(PropTypes.string).isRequired,
+    selectedKeys: PropTypes.arrayOf(PropTypes.string).isRequired,
+    popConfirmStatus: PropTypes.bool.isRequired,
+}
+
+export default DropdownComponent
